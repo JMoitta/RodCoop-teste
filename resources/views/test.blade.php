4ede288ee2bd518c71a1d5337eed426c7c434597
mex8063 <mex8063@24046-5636.INFO>
2018-12-03 16:12:26 -0200

    Aula 08 - Mais detalhes sobre a interpolação do Blade

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+Hello World!!!
+{{ $nome }}
+{{ $variavel1 }}
+<br/>
+{{ $variavel1 . ' ' . $nome}}
+<br/>
+{{ 2 + 2 }}
+<br/>
+{{ isset($test) ? $test : "outro valor"}}
+<br/>
+{{ $test ?? "outro valor"}}
+<br/>
+{{ $test or "outro valor"}}
+<br><br><br><br><br><br>
+{{ "<h1>School of Net</h1>"}}
+{!! "<h1>School of Net</h1>" !!}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,78 @@
 |
 */
 
+use Illuminate\Http\Request;
+
+//MVC - Model - View - Controller
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //helper
 });
+
+Route::get('/blade', function() {
+    $nome = "Luiz Carlos";
+    $variavel1 = "valor1";
+
+    return view('test')
+        ->with('nome', $nome)
+        ->with('variavel1', $variavel1)
+        ->with('test', 'Tenho valor ');
+});
+
+Route::get('/cliente/cadastrar', function(Request $request){
+    $nome = "Luiz Carlos";
+    $variavel1 = "valor1";
+
+    return view('cliente.cadastrar')
+        ->with('nome', $nome)
+        ->with('variavel1', $variavel1);
+
+    // return view('cliente.cadastrar', compact('nome', 'variavel1'));
+
+    // return view('cadastrar', [
+    //     'nome' => 'Luiz Carlos',
+    //     'variavel1' => 'valor1'
+    // ]);
+});
+
+/*Route::get('/cliente', function () {
+    //csrf-token
+    $csrfToken=csrf_token();
+    $html = <<<HTML
+<html>
+<body>
+    <h1>Cliente</h1>
+    <form method="post" action="/cliente/cadastrar">
+        <input type="hidden" name="_token" value="$csrfToken">
+        <input type="text" name="name"/>
+        <button type="submit">Enviar</button> 
+    </form>
+</body>
+</html>
+HTML;
+    return $html;
+});
+
+Route::post('/cliente/cadastrar', function(Request $request){
+    echo $request->get('name');
+    echo $request->name;
+});*/
+
+
+/*Route::get('/admin/cliente', function() {
+    return 'Admin - Hello World!';
+});
+
+Route::get('/cliente-echo', function() {
+    return 'Texto com echo';
+});
+
+Route::get('/produto/{name}/{id}', function($name, $id) {
+    return "Produto $name - $id";
+});
+
+Route::get('/fornecedor/{name}/{id?}', function ($name, $id = 1000) {
+    return "Fornecedor $name - $id";
+});*/
+
+//CoC Convention over Configuration
