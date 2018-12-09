7eb1c9b7000b0bc47a08416cab47bf60fb50baf8
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-12-09 19:04:54 -0200

    Iniciando com Eloquent ORM

---
+++ resources/views/aula/video-14.blade.php
@@ -0,0 +1,16 @@
+<h1>Iniciando com Eloquent ORM</h1>
+
+<p>Configuração de acesso ao banco de dados .env</p>
+
+DB_CONNECTION=<b>pgsql</b><br/>
+DB_HOST=<b>127.0.0.1</b><br/>
+DB_PORT=<b>5432</b><br/>
+DB_DATABASE=<b>son_laravel_55</b><br/>
+DB_USERNAME=<b>postgres</b><br/>
+DB_PASSWORD=<b>admin</b><br/>
+
+
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan make:model Models/Client</li>
+</ul>
