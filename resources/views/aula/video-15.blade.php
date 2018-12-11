0cd98a680498c56dbdaa092f087723b489b6c1b1
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-12-11 21:57:43 -0200

    Iniciando com sistema de migrations

---
+++ resources/views/aula/video-15.blade.php
@@ -0,0 +1,10 @@
+<h1>Iniciando com sistema de migrations</h1>
+
+<p>Alterar arquivo php.ini descomentar linha do <i>extension=pdo_pgsql</i></p>
+
+
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan migrate</li>
+    <li>php artisan migrate:refresh</li>
+</ul>
