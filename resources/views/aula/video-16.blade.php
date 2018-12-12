4b142a3cf99957a50431625880baad1af1feac72
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-12-11 23:35:21 -0200

    Prática com sistema de migrações

---
+++ resources/views/aula/video-16.blade.php
@@ -0,0 +1,12 @@
+<h1>Prática com sistema de migrações</h1>
+
+
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan make:migration create_clients_table --create=clients </li>
+    <li>php artisan migrate</li>
+    <li>php artisan migrate:refresh</li>
+    <li>php artisan migrate:status</li>
+    <li>php artisan migrate:reset</li>
+    <li>php artisan migrate:fresh</li>
+</ul>
