13b81137aceb51b1cdf3bb460183f31e1e57d6db
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-12-13 23:30:00 -0200

    Prática com Eloquent ORM

---
+++ resources/views/aula/video-17.blade.php
@@ -0,0 +1,16 @@
+<h1>Prática com Eloquent ORM</h1>
+
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan tinker</li>
+    <ul>
+        <li>$client = new \App\Models\Client();</li>
+        <li>$client->name = 'Luiz Carlos';</li>
+        <li>$client->name</li>
+        <li>$client->email = 'luiz@son.com.br';</li>
+        <li>$client->email</li>
+        <li>$client->save()</li>
+        <li>$client</li>
+    </ul>
+</ul>
+
