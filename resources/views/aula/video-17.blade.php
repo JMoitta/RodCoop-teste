8ba10612d0b3bf08149247480a76167847c9a349
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-12-16 08:37:36 -0200

    Prática com Eloquent ORM

--- resources/views/aula/video-17.blade.php
+++ resources/views/aula/video-17.blade.php
@@ -11,6 +11,17 @@
         <li>$client->email</li>
         <li>$client->save()</li>
         <li>$client</li>
+        <li>$clients = \App\Models\Client::all()</li>
+        <li>$clients[0]</li>
+        <li>$clients = \App\Models\Client::find(1)</li>
+        <li>$clients = \App\Models\Client::find(2)</li>
+        <li>$client->name = "Luiz Carlos Diniz"</li>
+        <li>$client->save()</li>
+        <li>$client</li>
+        <li>$client->delete()</li>
+        <li>$clients = \App\Models\Client::find(1)</li>
+        <li>$clients = \App\Models\Client::all()</li>
+        <li>count($clients)</li>
     </ul>
 </ul>
 
