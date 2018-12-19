8bab6347ae5304e7a50c4773b584f38f0bf494e0
mex8063 <mex8063@24046-5636.INFO>
2018-12-19 08:23:41 -0200

    Brincando com sessao e cookies

---
+++ resources/views/laravel54_auth/video-04.blade.php
@@ -0,0 +1,15 @@
+<h1>Brincando com sessão e cookies</h1>
+
+<h3>HTTP</h3>
+<p>Não mantém estado</p>
+
+<h3>Sessão</h3>
+<p>Guardada no servidor - os dados do usuário</p>
+<p>Cokkie da sessão</p>
+
+<h3>Cokkies</h3>
+<p>Guardado no browser do cliente</p>
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan serve</li>
+</ul>
