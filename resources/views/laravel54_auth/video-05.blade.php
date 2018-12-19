d1b98030bdf160dd3bc6e32b247bc7ce584b4388
mex8063 <mex8063@24046-5636.INFO>
2018-12-19 09:01:22 -0200

    Remember me

---
+++ resources/views/laravel54_auth/video-05.blade.php
@@ -0,0 +1,14 @@
+<h1>Remember me</h1>
+
+<h3>SESSION</h3>
+<p>Configurações da sessão no arquivos config/session.php:</p>
+<ul>
+  <li><b>'encrypt' => true</b>: criptografa a sessão do usuário</li>
+  <li><b>'files' => storage_path('framework/sessions')</b>: define a pasta onde será salva as sessões dentro da pasta storage/.</li>
+  <li><b></b></li>
+</ul>
+
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan serve</li>
+</ul>
