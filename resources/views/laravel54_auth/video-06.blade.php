620ea5651336aea4658784365f5b5c1ebea47d97
mex8063 <mex8063@24046-5636.INFO>
2018-12-20 13:39:50 -0200

    Traduzindo estrutura de autenticacao

---
+++ resources/views/laravel54_auth/video-06.blade.php
@@ -0,0 +1,12 @@
+<h1>Traduzindo estrutura de autenticação</h1>
+
+<h3>CMD</h3>
+<ul>
+  <li><b>composer require caouecs/laravel-lang</b> (Obs.: biblioteca de tradução da aplicação)</li>
+
+</ul>
+
+<p>Comandos ultilizados</p>
+<ul>
+    <li>php artisan serve</li>
+</ul>
