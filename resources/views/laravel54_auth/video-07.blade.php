087e02fa29c0d602c155f952d3cabf1d446e7ad3
mex8063 <mex8063@24046-5636.INFO>
2018-12-21 13:55:40 -0200

    Recuperando de senha

---
+++ resources/views/laravel54_auth/video-07.blade.php
@@ -0,0 +1,20 @@
+<h1>Recuperando de senha</h1>
+
+<h3>.env</h3>
+<ul>
+  <li>APP_URL=http://localhost:8000</li>
+  <li></li>
+  <li>MAIL_DRIVER=smtp</li>
+  <li>MAIL_HOST=smtp.mailtrap.io</li>
+  <li>MAIL_PORT=2525</li>
+  <li>MAIL_USERNAME=?</li>
+  <li>MAIL_PASSWORD=?</li>
+  <li>MAIL_ENCRYPTION=null</li>
+</ul>
+
+<h3>CMD</h3>
+<ul>
+  <li>php artisan serve</li>
+  <li><b>composer require caouecs/laravel-lang</b> (Obs.: biblioteca de tradução da aplicação)</li>
+  <li>php artisan make:notification MyResetPasswordNotification</li>
+</ul>
