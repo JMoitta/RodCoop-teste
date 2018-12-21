da1011298dfeaf3e03954866dc21219e682a88ec
mex8063 <mex8063@24046-5636.INFO>
2018-12-21 14:17:56 -0200

    Aprofundando em rotas de autenticacao

---
+++ resources/views/laravel54_auth/video-08.blade.php
@@ -0,0 +1,14 @@
+<h1>Aprofundando em rotas de autenticação</h1>
+
+<h3>.env</h3>
+<ul>
+  <li>APP_ENV=local</li>
+</ul>
+
+<h3>CMD</h3>
+<ul>
+  <li>composer require barryvdh/laravel-ide-helper --dev</li>
+  <li>php artisan serve</li>
+  <li>php artisan ide-helper:generate</li>
+  <li>php artisan ide-helper:meta (PHPStorm)</li>
+</ul>
