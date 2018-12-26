cc5e68e507dff913b1e01c283eb8bbaff496a3fe
mex8063 <mex8063@24046-5636.INFO>
2018-12-26 12:57:36 -0200

    Autenticacao e multiplas areas

---
+++ resources/views/laravel54_auth/video-09.blade.php
@@ -0,0 +1,15 @@
+<h1>Autenticação e múltiplas áreas</h1>
+
+<h3>composer.json</h3>
+<p>Adicionar em autoload:</p>
+<ul>
+  <li><pre>"files" : [
+    "app/helpers.php"
+],</pre>
+    </li>
+</ul>
+
+<h3>CMD</h3>
+<ul>
+  <li>composer dumpautoload</li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
     $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
     $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
     $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+
+    $this->get('/home', 'HomeController@index')->name('home');
 });
 
 Route::group(['prefix' => '/'], function () {
