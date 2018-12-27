9875717733360899187b57fd84764b43bb9ba5f1
mex8063 <mex8063@24046-5636.INFO>
2018-12-27 13:07:32 -0200

    Adicionando campos no registro de usuario

--- resources/views/laravel54_auth/video-11.blade.php
+++ resources/views/laravel54_auth/video-11.blade.php
@@ -1,23 +1,10 @@
 <h1>Adicionando campos no registro de usuário</h1>
 
-<h3>composer.json</h3>
-<p>Adicionar em autoload:</p>
-<ul>
-  <li><pre>"files" : [
-    "app/helpers.php"
-],</pre>
-    </li>
-</ul>
-
 <h3>CMD</h3>
 <ul>
   <li>php artisan make:migration add_phone_and_cpf_to_users --table=users</li>
   <li>php artisan migrate:refresh --seed</li>
   <li>composer require codeedu/code_validator:0.0.2</li>
-
-
-
-  <li>php artisan make:seeder UsersTableSeeder</li>
 </ul>
 
 <h3>Tinker</h3>
